45214171934ae487a043a3a1ab1e2b9ef6c61293
Hafizh Raihan <hafizhr205@gmail.com>
2026-01-14 14:00:25 +0700

    tambah docker

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$koneksi = new mysqli(
-    "localhost",     // host
-    "root",          // username
-    "",              // password (default XAMPP/Laragon kosong)
-    "warungajib1"    // nama database
-);
+$host = getenv('DB_HOST') ?: 'db';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: 'root';
+$db   = getenv('DB_NAME') ?: 'warungajib1';
+
+$koneksi = new mysqli($host, $user, $pass, $db);
 
 if ($koneksi->connect_error) {
     die("DB ERROR: " . $koneksi->connect_error);
